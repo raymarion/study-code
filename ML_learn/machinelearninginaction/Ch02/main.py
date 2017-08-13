90ea414acedcd02a299331b2799713f557dd4232
zhourui <zhourui@smartdevices.com.cn>
2017-08-13 18:59:00 +0800

    add ml in action code

---
+++ ML_learn/machinelearninginaction/Ch02/main.py
@@ -0,0 +1,14 @@
+import kNN
+
+
+# datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
+#
+# print datingDataMat
+# print datingLabels
+
+if __name__ == '__main__':
+    print "ML in action CH 02"
+
+    # kNN.datingClassTest()
+    kNN.handwritingClassTest()
+
